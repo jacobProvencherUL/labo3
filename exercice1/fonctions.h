80948438b17b6a950736489c8ac38624af77f188
jacobProvencherUL <japro72@ulaval.ca>
2026-02-02 07:30:36 -0500

    mon premier commit

---
+++ exercice1/fonctions.h
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   fonctions.h
+ * Author: etudiant
+ *
+ * Created on February 1, 2026, 8:45 a.m.
+ */
+
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+
+#include <array>
+
+constexpr int NOMBRES_LANCES{10};
+constexpr int NOMBRE_FACES{6};
+
+int 
+
+#endif /* FONCTIONS_H */
+
---
+++ exercice1/principal.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: etudiant
+ *
+ * Created on February 1, 2026, 8:44 a.m.
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
